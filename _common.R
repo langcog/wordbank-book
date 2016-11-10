43fda896ecec4b8b98ba25c62291daaabb915c3f
Mika Braginsky <mikabr@stanford.edu>
2016-11-10 11:07:59 -0500

    gender analysis ported, knitr and theme settings in common script

---
+++ _common.R
@@ -0,0 +1,12 @@
+knitr::opts_chunk$set(
+  message = FALSE,
+  warning = FALSE,
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  fig.align = "center",
+  fig.show = "hold"
+)
+
+ggplot2::theme_set(langcog::theme_mikabr())
+font <- langcog::theme_mikabr()$text$family
